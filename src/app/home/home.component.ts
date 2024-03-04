ecc258be2898c89e32fe65827695ec0263157b16
justice169 <6020345v@gmail.com>
2024-03-04 14:24:16 +0700

    git init

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,8 +14,17 @@ export class HomeComponent {
 
   picitems: PicItem[] = [
     {
-      name: 'pic1',
+      name: 'pic0',
       image: 'OM075_Main-Banner-1440x472.jpg'
     },
+    {
+      name: 'pic1',
+      image: 'OM026-Herobanner-1024x475-01.jpg'
+    },
+    {
+      name: 'pic2',
+      image: '1200x1200px-health-article-knowledge-warning-signs-hyperlipidemia-high-blood-fat.jpg'
+    }
+  
   ]
 }
